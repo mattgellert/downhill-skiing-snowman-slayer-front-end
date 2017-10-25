fd7676ac8ebb91bd453864623bf05a474e447267
mattgellert <mattgellert@gmail.com>
2017-10-25 17:37:26 -0400

    OOP MVP complete

---
+++ public/src/app.js
@@ -0,0 +1,202 @@
+class App {
+  constructor() {
+
+  }
+
+  static init() {
+    const startForm = document.querySelector('form#start')
+    startForm.innerHTML = "<input type='submit' value='New Game'></input>"
+    startForm.addEventListener('submit', (e) => {
+      e.preventDefault()
+      startForm.innerHTML = ''
+      this.startGame();
+    })
+  }
+
+  static startGame() {
+    this.game = new Game()
+    this.game.start()
+  }
+}
+
+
+class Game {
+  constructor() {
+    this.canvas = document.createElement("canvas")
+    this.canvas.width = 480;
+    this.canvas.height = 540;
+    this.context = this.canvas.getContext("2d");
+
+
+    this.updateGameArea = this.updateGameArea.bind(this)
+  }
+
+  start() {
+    document.body.insertBefore(this.canvas, document.body.childNodes[0])
+    this.frameNo = 0;
+    this.background = new Component(480, 540, "images/bg.png", 0, 0, "background", this);
+    this.skier = new Skier(40, 40, "images/Skier.png", 220, 480, "image", this);
+    this.score = new Component("20px", "Consolas", "blue", 340, 40, "text", this);
+    this.snowmenScore = new Component("20px", "Consolas", "blue", 340, 80, "text", this);
+    this.interval = setInterval(this.updateGameArea, 5)
+    this.snowmen = [];
+    this.snowballs = [];
+    this.snowmenHit = 0;
+    this.snowballTimer = 14;
+
+    this.addKeyPressListeners();
+  }
+
+  clear() {
+    this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+  }
+
+  stop() {
+    clearInterval(this.interval);
+  }
+
+  updateGameArea() {
+    this.frameNo++
+    this.clear()
+
+    this.skier.updateSkierDirection();
+
+    this.background.speedY = 1;
+    this.background.newPos();
+    this.background.update();
+    this.skier.newPos();
+    this.skier.update();
+
+    this.snowballs.forEach(snowball => {
+      snowball.y -= 5
+      snowball.update()
+    })
+
+    let newSnowmen = this.snowmanCreator(1.05) 
+    this.snowmen = [...this.snowmen, ...newSnowmen]
+    console.log(this.snowmen.length)
+
+    this.detectCollisions(this.snowmen)
+
+    this.score.text="SCORE: " + this.frameNo;
+    this.score.update();
+
+    this.snowmenScore.text="Snowmen: " + this.snowmenHit;
+    this.snowmenScore.update()
+
+    this.snowballTimer++;
+  }
+
+  addKeyPressListeners() {
+    document.addEventListener('keydown', (e) => {
+      e.preventDefault();
+      if (e.which === 32) {
+        if (this.snowballTimer > 14) {
+          let snowball = new Component(9, 15, "images/Snowball.png", this.skier.x + 30, this.skier.y + 20, "image", this);
+          this.skier.image.src = "images/ForwardThrow/SkierForwardThrow6.png";
+          this.snowballs.push(snowball);
+          this.snowballTimer = 0;
+        }
+      } else {
+        this.keys = (this.keys || []);
+        this.keys[e.keyCode] = (e.type === 'keydown');
+
+      }
+    });
+
+    document.addEventListener('keyup', (e) => {
+      if (e.which !== 32) {
+        this.keys[e.keyCode] = (e.type === 'keydown');
+        this.clearMove();
+      }
+    });
+  }
+
+  clearMove() {
+    this.skier.image.src = "images/Skier.png";
+  }
+
+  snowmanCreator(difficulty) {
+    let num = Math.floor(Math.random() * difficulty);
+    let newSnowmen = [];
+    for (var i = 0; i < num; i++) {
+      newSnowmen.push(new Component(40, 60, "images/Snowman1.png", Math.floor(Math.random() * this.background.width), 0, "image", this));
+    }
+    return newSnowmen;
+  }
+
+  detectCollisions(snowmen) {
+    snowmen = snowmen.filter(snowman => {
+      snowman.speedY = 1;
+      snowman.newPos()
+      snowman.update()
+
+      this.detectSnowballCollision(snowman)
+      this.detectSkierCollision(snowman, "snowman");
+
+      snowman.y > this.background.height ? false : true
+    })
+  }
+
+  detectSnowballCollision(snowman) {
+    this.snowballs = this.snowballs.filter(snowball => {
+
+      let withinY = (snowball.y <= (snowman.y + snowman.height) && snowball.y >= snowman.y)
+      let upperLeftwithinX = (snowball.x >= snowman.x && snowball.x <= (snowman.x + snowman.width))
+      let upperRightwithinX = (snowball.x + snowball.width) >= snowman.x && (snowball.x + snowball.width) <= (snowman.x + snowman.width)
+
+      if (withinY && (upperLeftwithinX || upperRightwithinX)) {
+        snowman.image.src = "images/SnowmanDeath2.png"
+        setTimeout(function(){
+          snowman.height = 0
+          snowman.width = 0
+        }, 200)
+        this.snowmenHit++
+        return false
+      }
+      if (snowball.y < 0) {
+        return false
+      } else {
+        return true
+      }
+    });
+  }
+
+  detectSkierCollision(component, type) {
+    let withinY = ((this.skier.y + 12) <= (component.y + component.height) && (this.skier.y + 12) >= component.y)
+    let upperLeftwithinX = ((this.skier.x + 14) >= component.x && (this.skier.x + 14) <= (component.x + component.width))
+    let upperRightwithinX = (this.skier.x + this.skier.width - 14) >= component.x && (this.skier.x + this.skier.width - 14) <= (component.x + component.width)
+
+    if (withinY && (upperLeftwithinX || upperRightwithinX)) {
+      if (type === "snowman") {
+        this.endGame();
+      } else if (type === "tree") {
+        // ADD TREE LOGIC
+      }
+    }
+  }
+
+  endGame() {
+    this.stop();
+    // const startForm = document.querySelector('form#start')
+    // const startButton = document.createElement('input')
+    // startButton.type = "submit"
+    // startButton.value = "New Game"
+    setTimeout(() => {
+      document.body.removeChild(document.querySelector('canvas'))
+      this.resetGameComponents();
+      this.displayScore();
+      App.init();
+    }, 1000);
+  };
+
+  resetGameComponents() {
+    this.snowmen = [];
+    this.snowballs = [];
+    this.snowmenHit = 0;
+  }
+
+  displayScore() {
+    console.log("user score:",parseInt(this.score.text.slice(7)))
+  }
+}
--- public/src/index.js
+++ public/src/index.js
@@ -1,71 +1,11 @@
-$(document).ready(() => {
-  const leaderboardWrapper = document.getElementById('leaderboard-wrapper')
-  const leaderboard
-  fetch('http://localhost:3000/api/v1/users')
-    .then(resp => resp.json())
-    .then(json => displayLeaders(json))
-
-  function displayLeaders(json) {
-    let leaders = json.sort((a,b) => {
-      if (a.top_score > b.top_score) {
-        return -1
-      } else if (a.top_score === b.top_score) {
-        return 0
-      } else {
-        return 1
-      }
-    })
-    debugger
-  }
-
-
-  const startForm = document.querySelector('form#start')
-  const startButton = document.createElement('input')
-  startButton.type = "submit"
-  startButton.value = "New Game"
-  startForm.appendChild(startButton)
-  startForm.addEventListener('submit', function(e) {
-    e.preventDefault()
-    startForm.removeChild(startButton)
-    startGame();
-  })
-
-  let myBackground;
-  let mySkier;
-  let myScore;
-  let mySnowmenScore;
-
-  function startGame() {
-    myBackground = new Component(480, 540, "images/bg.png", 0, 0, "background");
-    mySkier = new Component(40, 40, "images/Skier.png", 220, 480, "image");
-    myScore = new Component("20px", "Consolas", "blue", 340, 40, "text");
-    mySnowmenScore = new Component("20px", "Consolas", "blue", 340, 80, "text");
-    myGameArea.start();
-  };
-
-  let myGameArea = {
-    canvas : document.createElement("canvas"),
-    start : function() {
-      this.canvas.width = 480;
-      this.canvas.height = 540;
-      this.context = this.canvas.getContext("2d");
-      document.body.insertBefore(this.canvas, document.body.childNodes[0]);
-      this.frameNo = 0;
-      this.interval = setInterval(updateGameArea, 10);
-    },
-    clear : function() {
-      this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
-    },
-    stop : function() {
-      clearInterval(this.interval);
-    }
-  };
-
-  function Component(width, height, source, x, y, type) {
+class Component {
+  constructor(width, height, source, x, y, type, game) {
     this.type = type;
     if (type == "image" || type == "background") {
       this.image = new Image();
       this.image.src = source;
+    } else {
+      this.source = source
     }
     this.width = width;
     this.height = height;
@@ -73,190 +13,68 @@ $(document).ready(() => {
     this.speedY = 0;
     this.x = x;
     this.y = y;
-    this.update = function() {
-      ctx = myGameArea.context;
-      if (type == "image" || type == "background") {
-        ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
-        if (type == "background") {
-          ctx.drawImage(this.image, this.x, this.y - this.height, this.width, this.height);
-        }
-        } else {
-          ctx.fillStyle = source;
-          ctx.fillRect(this.x, this.y, this.width, this.height);
-        }
-        if (this.type == "text") {
-          ctx.font = this.width + " " + this.height;
-          ctx.fillStyle = source;
-          ctx.fillText(this.text, this.x, this.y);
-        }
-      }
-      this.newPos = function() {
-        this.x += this.speedX;
-        this.y += this.speedY;
-        if (this.type == "background") {
-          if (this.y == this.height) {
-            this.y = 0;
-          };
-        };
-      };
-    };
+    this.context = game.context;
+    this.game = game;
 
-  function updateSkierDirection() {
-    if (myGameArea.keys && myGameArea.keys[37]) {
-      if (mySkier.x > 0) {
-        mySkier.image.src = "images/LeftTurn/SkierTurnLeft3.png";
-        mySkier.x -= 2;
-      };
-    };
-    if (myGameArea.keys && myGameArea.keys[39]) {
-      if (mySkier.x < (myBackground.width - 40)){
-        mySkier.image.src = "images/RightTurn/SkierTurnRight3.png";
-        mySkier.x += 2;
-      };
-    };
-  };
-
-  let snowmen = [];
-  let snowballs = [];
-  let snowmenHit = 0;
-
-  function updateGameArea() {
-    myGameArea.frameNo++
-    myGameArea.clear();
-
-    updateSkierDirection();
-
-    myBackground.speedY = 1;
-    myBackground.newPos();
-    myBackground.update();
-    mySkier.newPos()
-    mySkier.update()
-
-    snowballs.forEach(snowball => {
-      snowball.y -= 5
-      snowball.update()
-    })
-
-    let newSnowmen = snowmanCreator(1.05)
-    snowmen = [...snowmen, ...newSnowmen]
-
-    detectCollisions(snowmen);
-
-    myScore.text="SCORE: " + myGameArea.frameNo;
-    myScore.update();
-
-    mySnowmenScore.text="Snowmen: " + snowmenHit;
-    mySnowmenScore.update()
-
-    snowballTimer++;
   }
 
-  function detectCollisions(snowmen) {
-    snowmen = snowmen.filter(snowman => {
-      snowman.speedY = 1;
-      snowman.newPos()
-      snowman.update()
-
-      detectSnowballCollision(snowman)
-      detectSkierCollision(snowman, "snowman");
-
-      snowman.y > myBackground.height ? false : true
-    })
-  };
-
-  function detectSnowballCollision(snowman) {
-    snowballs = snowballs.filter(snowball => {
-
-      let withinY = (snowball.y <= (snowman.y + snowman.height) && snowball.y >= snowman.y)
-      let upperLeftwithinX = (snowball.x >= snowman.x && snowball.x <= (snowman.x + snowman.width))
-      let upperRightwithinX = (snowball.x + snowball.width) >= snowman.x && (snowball.x + snowball.width) <= (snowman.x + snowman.width)
-
-      if (withinY && (upperLeftwithinX || upperRightwithinX)) {
-        snowman.image.src = "images/SnowmanDeath2.png"
-        setTimeout(function(){
-          snowman.height = 0
-          snowman.width = 0
-        }, 200)
-        snowmenHit++
-        return false
-      }
-      if (snowball.y < 0) {
-        return false
-      } else {
-        return true
-      }
-    });
-  };
-
-  function detectSkierCollision(component, type) {
-    let withinY = (mySkier.y <= (component.y + component.height) && mySkier.y >= component.y)
-    let upperLeftwithinX = (mySkier.x >= component.x && mySkier.x <= (component.x + component.width))
-    let upperRightwithinX = (mySkier.x + mySkier.width) >= component.x && (mySkier.x + mySkier.width) <= (component.x + component.width)
-
-    if (mySkier.x >= component.x && mySkier.x <= (component.x + component.width) && (component.y + 50) === mySkier.y) {
-      if (type === "snowman") {
-        endGame();
-      } else if (type === "tree") {
-        // ADD TREE LOGIC
-      }
-    }
-  }
-
-  function snowmanCreator(difficulty) {
-    let num = Math.floor(Math.random() * difficulty);
-    let newSnowmen = [];
-    for (var i = 0; i < num; i++) {
-      newSnowmen.push(new Component(40, 60, "images/Snowman1.png", Math.floor(Math.random() * myBackground.width), 0, "image"));
-    }
-    return newSnowmen;
-  };
-
-  let snowballTimer = 14;
-  document.addEventListener('keydown', function(e) {
-    e.preventDefault();
-    if (e.which === 32) {
-      if (snowballTimer > 14) {
-        let snowball = new Component(9, 15, "images/Snowball.png", mySkier.x + 30, mySkier.y + 20, "image");
-        mySkier.image.src = "images/ForwardThrow/SkierForwardThrow6.png";
-        snowballs.push(snowball);
-        snowballTimer = 0;
+  update() {
+    const ctx = this.context;
+    if (this.type == "image" || this.type == "background") {
+      ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
+      if (this.type == "background") {
+        ctx.drawImage(this.image, this.x, this.y - this.height, this.width, this.height);
       }
     } else {
-      myGameArea.keys = (myGameArea.keys || []);
-      myGameArea.keys[e.keyCode] = (e.type == 'keydown');
+      ctx.fillStyle = this.source;
+      ctx.fillRect(this.x, this.y, this.width, this.height);
     }
-  });
-
-  document.addEventListener('keyup', function(e) {
-    if (e.which !== 32) {
-      myGameArea.keys[e.keyCode] = (e.type == 'keydown');
-      clearmove();
+    if (this.type == "text") {
+      ctx.font = this.width + " " + this.height;
+      ctx.fillStyle = this.source;
+      ctx.fillText(this.text, this.x, this.y);
     }
-  });
-
-  function clearmove() {
-      mySkier.image.src = "images/Skier.png";
-  };
-
-  function endGame() {
-    myGameArea.stop();
-    setTimeout(function() {
-      document.body.removeChild(document.querySelector('canvas'))
-      // postScore();
-      resetGameComponents();
-      displayScore();
-      startForm.appendChild(startButton)
-    }, 1000);
-  };
-
-  function resetGameComponents() {
-    snowmen = [];
-    snowballs = [];
-    snowmenHit = 0;
-  };
-
-  function displayScore() {
-    console.log("user score:",parseInt(myScore.text.slice(7)))
   }
 
+  newPos() {
+    this.x += this.speedX;
+    this.y += this.speedY;
+    if (this.type == "background") {
+      if (this.y == this.height) {
+        this.y = 0;
+      };
+    };
+  };
+};
+
+
+
+class Skier extends Component {
+  constructor(width, height, source, x, y, type, game) {
+    super(width, height, source, x, y, type, game)
+  }
+
+
+  updateSkierDirection() {
+    if (this.game.keys && this.game.keys[37]) {
+      if (this.x > 0) {
+        this.image.src = "images/LeftTurn/SkierTurnLeft3.png";
+        this.x -= 2;
+      };
+    };
+    if (this.game.keys && this.game.keys[39]) {
+      if (this.x < (this.game.background.width - 40)){
+        this.image.src = "images/RightTurn/SkierTurnRight3.png";
+        this.x += 2;
+      };
+    };
+  };
+
+}
+
+
+
+$(document).ready(() => {
+  const app = new App()
+  App.init()
 });
